c5577108cd37304b44effd16c38be0c51c5e3f0d
Amirali <amiiraliborji1388@gmail.com>
2026-07-01 22:24:09 +0330

    Change Portfolio & Articles Font

--- Frontend/src/pages/Portfolio/Portfolio.jsx
+++ Frontend/src/pages/Portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ function Portfolio() {
       <div className="pt-16 md:pt-24 bg-linear-to-t from-blue-50 to-indigo-100 dark:from-slate-900 dark:to-slate-800">
         <div className="container">
           <div className="max-w-3xl mx-auto text-center">
-            <h1 className="mb-6 text-4xl font-bold text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <h1 className="mb-6 text-4xl font-Morabba-Bold  text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
               نمونه کارها
             </h1>
 
